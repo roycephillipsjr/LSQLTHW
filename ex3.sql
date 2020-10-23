5c4f22333b68b08cba10a4bd6a046dd99bbf981d
Royce_Phillips_Jr <roycephillipsjr@gmail.com>
2020-10-23 11:54:16 -0500

    redoing exercises

---
+++ ex3.sql
@@ -0,0 +1,13 @@
+INSERT INTO person (id, first_name, last_name, age)
+  VALUES (0, 'Zed', 'Shaw', 37);
+
+INSERT INTO pet (id, name, breed, age, dead)
+  VALUES (0, 'Fluffy', 'Unicorn', 1000, 0);
+
+INSERT INTO pet VALUES (1, 'Gigantor', 'Robot', 1, 1);
+
+INSERT INTO person (id, first_name, last_name, age)
+  VALUES (1, 'Royce', 'Phillips', 31);
+
+INSERT INTO pet (id, name , breed, age, dead)
+  VALUES (2, 'Tony', 'Tiger', 12, 0);

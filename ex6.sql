d91980d4ac378d14a9d1885d8721a389369d5a42
Royce_Phillips_Jr <roycephillipsjr@gmail.com>
2020-10-28 12:55:06 -0500

    adding exercises 5,6,7

---
+++ ex6.sql
@@ -0,0 +1,27 @@
+/* normal join with equality */
+SELECT pet.id, pet.name, pet.age, pet.dead
+  FROM pet, person_pet, person
+  WHERE
+  pet.id = person_pet.pet_id AND
+  person_pet.person_id = person.id AND
+  person.first_name = 'Zed';
+
+/* using a sub-select */
+SELECT pet.id, pet.name, pet.age, pet.dead
+  FROM pet
+  WHERE pet.id IN
+  (
+    SELECT pet_id FROM person_pet, person
+    WHERE person_pet.person_id = person.id
+    AND person.first_name = 'Zed'
+  );
+ -- Adding my pets
+ SELECT pet.*
+  FROM pet
+  WHERE pet.id IN
+  (
+    SELECT pet_id
+    FROM person_pet, person
+    WHERE person_pet.person_id = person.id
+    AND person.id = 1
+  );
